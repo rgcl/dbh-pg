2c14c18d0e1e029dae0b7c881d9010227c93bf4f
roro89 <rodrigo.gonzlez@gmail.com>
2014-05-04 12:41:57 -0400

    test

--- test/db.js
+++ test/db.js
@@ -7,7 +7,23 @@
 'use strict';
 
 var assert = require("assert");
+var DBH = require('../');
 
 describe('DBH', function() {
-    // TODO
-})
+    describe('Instantiate BDH', function() {
+        
+        it('instantiate DBH to db', function() {
+            var db = new BDH({
+                host: process.argv[2] || 'localhost',
+                port: process.argv[3] || '5432',
+                user: process.argv[4] || 'postgres',
+                password: process.argv[5] || null,
+                database: process.argv[6] || 'db2test'
+            });
+        });
+        
+        it('db is instance of DBH', function() {
+            assert.equal(db instanceof DBH, true);
+        });
+    });
+});
